a0bdea9260c5fa93c283d95c3949127acd008146
Mr-Duo <duong.nd215336@sis.hust.edu.vn>
2026-06-17 12:08:11 +0700

    Add jitfine, la, lr

--- models/init_model.py
+++ models/init_model.py
@@ -1,8 +1,11 @@
 from .deepjit.warper import DeepJIT
 from .tlel.warper import TLELModel
 from .simcom.warper import SimCom
+from .lr.warper import LogisticRegression
+from .lapredict.warper import LAPredict
+from .jitfine.warper import JITFine
 
-models = ["deepjit", "simcom", "lapredict", "tlel", "lr", "jitfine", "vccfinder"]
+models = ["deepjit", "simcom", "lapredict", "tlel", "lr", "jitfine"]
 
 def init_model(model_name, language, device):   
     if  model_name == "deepjit":
@@ -11,5 +14,12 @@ def init_model(model_name, language, device):
         return TLELModel(language=language)
     elif model_name == "simcom":
         return SimCom(language=language, device=device)
+    elif  model_name == "lapredict":
+        return LAPredict(language=language)
+    elif  model_name == "lr":
+        return LogisticRegression(language=language)
+    elif model_name == "jitfine":
+        return JITFine(language=language, device=device)
     else:
         raise Exception("No such model")
+    
